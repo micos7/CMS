7203adcc361ed1e7b42bc01401f79a52bfa917ce
micos7 <micos7@gmail.com>
2017-05-18 17:26:29 +0300

    display categories in admin

--- admin/categories.php
+++ admin/categories.php
@@ -1,9 +1,9 @@
-<?php include('includes/header.php'); ?>
+<?php include('includes/admin_header.php'); ?>
 
     <div id="wrapper">
 
         <!-- Navigation -->
-        <?php include('includes/navigation.php'); ?>
+        <?php include('includes/admin_navigation.php'); ?>
 
         <div id="page-wrapper">
 
@@ -29,6 +29,12 @@
                         </div>
 
                         <div class="col-xs-6">
+
+                        <?php 
+                             $query = "SELECT * FROM categories";
+                            $select_categories = mysqli_query($connection,$query);
+                        
+                        ?>
                         <table class="table table-bordered table-hover">
                             <thead>
                                 <tr>
@@ -37,10 +43,15 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>Test</td>
-                                    <td>Blah</td>
-                                </tr>
+                            <?php while($row = mysqli_fetch_assoc($select_categories)){
+                            $cat_id = $row['cat_id'];
+                            $cat_title = $row['cat_title'];
+                            echo "<tr>";
+                            echo "<td>{$cat_id}</td>";
+                            echo "<td>{$cat_title}</td>";
+                            echo "</tr>";
+                            } ?>
+                                
                                 
                             </tbody>
                         </table>
@@ -58,4 +69,4 @@
         <!-- /#page-wrapper -->
 
 
-<?php include('includes/footer.php'); ?>
+<?php include('includes/admin_footer.php'); ?>
