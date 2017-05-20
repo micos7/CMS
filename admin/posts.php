90ded8ef5bbf1a0ca9f969e9a8884316750dd1ae
micos7 <micos7@gmail.com>
2017-05-20 14:27:23 +0300

    Create Post form

--- admin/posts.php
+++ admin/posts.php
@@ -25,8 +25,8 @@
                        }
 
                        switch($source){
-                        case '34';
-                        echo "Nice";
+                        case 'add_post';
+                        include('includes/add_post.php');
                         break;
 
                         case '100';
