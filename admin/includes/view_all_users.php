b36878aa65362eda9a0d141dae70bb3b7c3576f3
micos7 <micos7@gmail.com>
2017-05-23 12:41:34 +0300

    Display users from db

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -2,60 +2,56 @@
                             <thead>
                                 <tr>
                                     <th>Id</th>
-                                    <th>Author</th>
-                                    <th>Comment</th>
+                                    <th>Username</th>
+                                    <th>Firstname</th>
+                                    <th>Lastname</th>
                                     <th>Email</th>
-                                    <th>Status</th>
-                                    <th>In Response to</th>
-                                    <th>Date</th>
-                                    <th>Approve</th>
-                                    <th>Unapprove</th>
-                                    <th>Delete</th>
+                                    <th>Role</th>
                                 </tr>
                             </thead>
                             <tbody>
                             <?php
 
-            $query = "SELECT * FROM comments";
-            $select_comments = mysqli_query($connection,$query);
+            $query = "SELECT * FROM users";
+            $select_users = mysqli_query($connection,$query);
 
-            while($row = mysqli_fetch_assoc($select_comments)){
-                            $comment_id = $row['comment_id'];
-                            $comment_post_id = $row['comment_post_id'];
-                            $comment_author = $row['comment_author'];
-                            $comment_email = $row['comment_email'];
-                            $comment_status = $row['comment_status'];
-                            $comment_content = $row['comment_content'];
-                            $comment_date = $row['comment_date'];
+            while($row = mysqli_fetch_assoc($select_users)){
+                            $user_id = $row['user_id'];
+                            $username = $row['username'];
+                            $user_password = $row['user_password'];
+                            $user_firstname = $row['user_firstname'];
+                            $user_lastname = $row['user_lastname'];
+                            $user_email = $row['user_email'];
+                            $user_image = $row['user_image'];
+                            $user_role = $row['user_role'];
     
 
                             echo "<tr>";
-                            echo "<td>$comment_id </td>";
-                            echo "<td>$comment_author </td>";
-                            echo "<td>$comment_content </td>";
-                            echo "<td>$comment_email </td>";
-                            echo "<td>$comment_status </td>";
+                            echo "<td>$user_id </td>";
+                            echo "<td>$username </td>";
+                            echo "<td>$user_firstname </td>";
+                            echo "<td>$user_lastname </td>";
+                            echo "<td>$user_email </td>";
+                            echo "<td>$user_role </td>";
 
 
-                    $ct = $connection->prepare("SELECT * FROM posts WHERE post_id=?");
-                    $ct->bind_param("i", $comment_post_id);
-                    $ct->execute();
-                    $post_ct = $ct->get_result();
-                    if(!$ct){
-                        printf("Error: %s.\n", $ep->error);
-                    }
+                    // $ct = $connection->prepare("SELECT * FROM posts WHERE post_id=?");
+                    // $ct->bind_param("i", $comment_post_id);
+                    // $ct->execute();
+                    // $post_ct = $ct->get_result();
+                    // if(!$ct){
+                    //     printf("Error: %s.\n", $ep->error);
+                    // }
                      
-                    while($row = $post_ct->fetch_assoc()){
-                        $post_id = $row['post_id'];
-                        $post_title = $row['post_title'];
-                    }
+                    // while($row = $post_ct->fetch_assoc()){
+                    //     $post_id = $row['post_id'];
+                    //     $post_title = $row['post_title'];
+                    // }
 
 
-                            echo "<td><a href='../post.php?p_id=$post_id'>$post_title</a></td>";
-                            echo "<td>$comment_date </td>";
-                            echo "<td><a href='comments.php?approve=$comment_id'>Approve</a></td>";
-                            echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";
-                            echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
+                            echo "<td><a href='comments.php?approve='>Approve</a></td>";
+                            echo "<td><a href='comments.php?unapprove='>Unapprove</a></td>";
+                            echo "<td><a href='comments.php?delete='>Delete</a></td>";
                             echo "</tr>";
 
              }
