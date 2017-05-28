61368df9cc6240b0ee4cbdc5fc14e3a2656475af
micos7 <micos7@gmail.com>
2017-05-28 11:50:48 +0300

    Registering,adding default salt and form validation

---
+++ registration.php
@@ -0,0 +1,82 @@
+<?php  include "includes/db.php"; ?>
+ <?php  include "includes/header.php"; ?>
+
+
+    <!-- Navigation -->
+    
+    <?php  include "includes/navigation.php"; ?>
+
+    <?php
+    if(isset($_POST['submit'])){
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $subscriber_role = 'subscriber';
+
+        if(!empty($username) && !empty($email) && !empty($password)){
+
+            $query = "SELECT randSalt FROM users";
+        $rand_salt = mysqli_query($connection,$query);
+
+        while($row = mysqli_fetch_assoc($rand_salt)){
+                $salt = $row['randSalt'];
+
+        }
+            $ap = $connection->prepare("INSERT INTO users(username, user_email, user_password,user_role)VALUES(?,?,?,?)");
+                                $ap->bind_param("ssss", $username,$email,$password,$subscriber_role);
+                                $ap->execute();
+
+            $message ='Your registration as been submitted!';
+
+        } else {
+            $message = 'Fields cannot be empty!';
+        }
+
+        
+    } else {
+        $message = '';
+    }
+    
+    
+     ?>
+    
+ 
+    <!-- Page Content -->
+    <div class="container">
+    
+<section id="login">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6 col-xs-offset-3">
+                <div class="form-wrap">
+                <h1>Register</h1>
+                    <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
+                    <h6 class="text-center"><?php echo $message; ?></h6>
+                        <div class="form-group">
+                            <label for="username" class="sr-only">username</label>
+                            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
+                        </div>
+                         <div class="form-group">
+                            <label for="email" class="sr-only">Email</label>
+                            <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                        </div>
+                         <div class="form-group">
+                            <label for="password" class="sr-only">Password</label>
+                            <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                        </div>
+                
+                        <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                    </form>
+                 
+                </div>
+            </div> <!-- /.col-xs-12 -->
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
+</section>
+
+
+        <hr>
+
+
+
+<?php include "includes/footer.php";?>
