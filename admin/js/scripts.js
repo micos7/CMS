c12ffa19b6cc36505353922206e5d7833c6417af
micos7 <micos7@gmail.com>
2017-05-29 18:11:54 +0300

    Added preloader

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -12,4 +12,10 @@ $('.checkBoxes').each(function(){
     });
 }
 })
+var div_box = '<div id="load-screen"><div id="loading"></div></div>';
+
+$("body").prepend(div_box);
+$("#load-screen").delay(700).fadeOut(600,function(){
+    $(this).remove();
+})
 });
