58989e40a677e540ef9b99052d02328f867e13ca
micos7 <micos7@gmail.com>
2017-06-08 13:37:25 +0300

    Active class for categories

--- includes/navigation.php
+++ includes/navigation.php
@@ -19,7 +19,17 @@
 
                 while($row = mysqli_fetch_assoc($select_all_cats)){
                     $cat_title = $row['cat_title'];
-                    echo "<li><a href='#'>$cat_title</a></li>";
+                    $cat_id = $row['cat_id'];
+                    $category_class ='';
+                    $registration_class='';
+                    $pageName = basename($_SERVER['PHP_SELF']);
+                    $registration  = 'registration.php';
+                    if(isset($_GET['category']) && $_GET['category'] == $cat_id){
+                        $category_class ='active';
+                    }else if($pageName == $registration ){
+                        $registration_class='active';
+                    }
+                    echo "<li class='$category_class'><a href='category.php?category=$cat_id'>$cat_title</a></li>";
                 }
                 
                 ?>
@@ -42,7 +52,7 @@
 
 
         
-                    <li>
+                    <li class="<?php echo $registration_class; ?>">
                         <a href="registration.php">Registration</a>
                     </li>
                     <li>
